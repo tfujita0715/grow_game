f41b55d12dc36ea15aaa54fd014b93bea60de0f9
hujita <t.fujita0715@gmail.com>
2026-03-02 13:38:18 +0900

    画面追加

---
+++ src/screens/DS01_title.py
@@ -0,0 +1,16 @@
+import pyxel
+
+from .base import Setting
+
+class Screen01(Setting):
+    def __init__(self):
+        super().__init__()
+        self.message = "This is sample"
+
+    def update(self):
+        self.update_common()
+
+    def draw(self):
+        pyxel.text(160, 120, "debug01",5)
+        pyxel.text(100, 120, self.message, 5)
+        self.draw_common()
