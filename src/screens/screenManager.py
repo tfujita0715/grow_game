7ce8a4f506de3017f8ae7f075e1b3261f946f953
hujita <t.fujita0715@gmail.com>
2026-03-02 14:10:21 +0900

    title画面更新

--- src/screens/screenManager.py
+++ src/screens/screenManager.py
@@ -2,11 +2,10 @@ import pyxel
 
 #画面インポート
 from .sampleScreen01 import Screen01
-
-
-#現在mainから01を直接呼んでいます。将来的にここから画面遷移を行いま。
-
-# from .settingScreen import SettingScreen 
+from .DS01_title import title
+from .base import Setting
+#ここで画面遷移を行います。
+# インポート例from .settingScreen import SettingScreen 
 
 class ScreenManager:
     @staticmethod
