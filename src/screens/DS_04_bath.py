f801cfe15e00545fdbd2f59fc06542a67d74bfbf
Yayuki <lukeendergy@gmail.com>
2026-04-06 21:35:41 +0900

    風呂整えた

--- src/screens/DS_04_bath.py
+++ src/screens/DS_04_bath.py
@@ -7,12 +7,19 @@ class Bath(BaseScreen):
     
     def update(self):
         if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
-            pass
+            #17,192,,,118,224
+            #140,193,,,240,225
+            #セキュリティ風呂ボタン
+            if (17 < pyxel.mouse_x < 118) and (192 < pyxel.mouse_y < 224):
+                pass
+            #色風呂ボタン
+            elif (140 < pyxel.mouse_x < 240) and (193 < pyxel.mouse_y < 225):
+                pass
 
     def draw(self):
         pyxel.load("bath.pyxres")
         pyxel.blt(0, 0, 0, 0, 0, 255, 255, 0)
-        pyxel.text(95, 123, "", 0)
-        pyxel.text(91, 230, "", 0)
+        pyxel.text(17, 192, "セキュリティ風呂", 0)
+        pyxel.text(140, 193, "色風呂", 0)
 
         
