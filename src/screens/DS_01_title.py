2b1540c442feef5a41a1c4942084fb2cbd18204a
hujita <t.fujita0715@gmail.com>
2026-04-09 01:46:38 +0900

    バグ修正

--- src/screens/DS_01_title.py
+++ src/screens/DS_01_title.py
@@ -8,11 +8,9 @@ class title(Popup):
         self.chara_data = chara_data
 
     def update(self):
-        self.update_common()
         if pyxel.btnp(pyxel.KEY_SPACE):
             self.next_screen = "room"
 
     def draw(self):
         pyxel.text(80, 120, "title", 20)
         pyxel.text(190, 120, "plz push space",5)
-        self.draw_common()
