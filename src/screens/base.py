f594a1e61435af169598d4df933c2d0ebef19d11
sora <225d0521@s.obirin.ac.jp>
2026-04-09 23:59:07 +0900

    popupボタンの追加と修正

--- src/screens/base.py
+++ src/screens/base.py
@@ -54,19 +54,27 @@ class Popup(BaseScreen):
         pyxel.rect(28, 28, 200, 200, 1)  #中身（紺）
         
         # テキストの表示
-        pyxel.text(35, 35, "メニュー", 10, self.font)
+        pyxel.text(35, 35, "メニュー", 7, self.font)
         #pyxel.text(45, 65, "This is a popup!", 7)
         #pyxel.text(45, 75, "[SPACE] to close", 13)
 
         #各ボタンの配置
-        pyxel.rect(40, 50, 50, 50, 7)
-        pyxel.rect(102, 50, 50, 50, 7)
-        pyxel.rect(164, 50, 50, 50, 7)
-        pyxel.rect(40, 110, 50, 50, 7)
-        pyxel.rect(102, 110, 50, 50, 7)
-        pyxel.rect(164, 110, 50, 50, 7)
-        pyxel.rect(71, 170, 50, 50, 7)
-        pyxel.rect(133, 170, 50, 50, 7)
+        pyxel.rect(40, 50, 50, 50, 6)
+        pyxel.text(53, 65, "へやに\n\nもどる", 7, self.font)
+        pyxel.rect(102, 50, 50, 50, 13)
+        pyxel.text(112, 72, "せってい", 7, self.font)
+        pyxel.rect(164, 50, 50, 50, 10)
+        pyxel.text(174, 72, "ショップ", 7, self.font)
+        pyxel.rect(40, 110, 50, 50, 8)
+        pyxel.text(45, 132, "びょういん", 7, self.font)
+        pyxel.rect(102, 110, 50, 50, 3)
+        pyxel.text(115, 132, "トイレ", 7, self.font)
+        pyxel.rect(164, 110, 50, 50, 12)
+        pyxel.text(177, 132, "おふろ", 7, self.font)
+        pyxel.rect(71, 170, 50, 50, 9)
+        pyxel.text(84, 192, "ごはん", 7, self.font)
+        pyxel.rect(133, 170, 50, 50, 0)
+        pyxel.text(140, 185, "じゅんび\n\nちゅう…", 7, self.font)
 
 
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
@@ -75,6 +83,8 @@ class Popup(BaseScreen):
 
             elif 101 < pyxel.mouse_x < 153 and 49 < pyxel.mouse_y < 101:
                 self.next_screen = "setting"
+
+
 
 
 
