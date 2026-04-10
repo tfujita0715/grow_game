f3dc76675c1029f5c9b09b3b56182feacea0a06b
sora <225d0521@s.obirin.ac.jp>
2026-04-11 00:54:39 +0900

    gameoverの修正

--- src/screens/DS_09_gameover.py
+++ src/screens/DS_09_gameover.py
@@ -6,6 +6,8 @@ class Gameover(BaseScreen):
     def __init__(self, game_data, chara_data):
         super().__init__(game_data, chara_data)
         self.chara_data = chara_data
+        self.font = pyxel.Font("misaki_gothic.bdf")
+
         
     def update(self):
         #タイトル画面に戻る処理
@@ -16,5 +18,5 @@ class Gameover(BaseScreen):
     def draw(self):
         #pyxel.bltm(0, 0, 0, 0, 0, 255, 255)
         pyxel.cls(0)
-        pyxel.text(70, 30, "GAME OVER" ,7)
-        pyxel.text(40, 220, "タイトルに戻る", 7)
+        pyxel.text(110, 120, "GAME OVER" ,7)
+        pyxel.text(100, 220, "タイトルに戻る", 7, self.font)
